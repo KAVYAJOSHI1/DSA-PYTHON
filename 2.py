65b21a6344ff0a46b56a05c866f2d01000851c34
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-12 20:35:10 +0530

    a

--- 2.py
+++ 2.py
@@ -6,3 +6,19 @@ KthLargest(int k, int[] nums) Initializes the object with the integer k and the 
 int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream."""
 
 
+import heapq
+
+class KthLargest:
+    def __init__(self, k, nums):
+        self.k = k
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+
+    def add(self, val):
+        if len(self.min_heap) < self.k:
+            heapq.heappush(self.min_heap, val)
+        elif val > self.min_heap[0]:
+            heapq.heapreplace(self.min_heap, val)
+        return self.min_heap[0]
